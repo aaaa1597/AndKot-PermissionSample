7a65c2fa49732a97dc7b68b7a1067ea71391fd89
aaaa1597 <tokushi2011@gmail.com>
2025-10-03 23:00:00 +0900

    Refine the authorization sequence for smarter processing. 権限承認のシーケンスをよりスマートな処理に修正。

--- app/src/main/java/com/aaa/permissionsample/MainActivity.kt
+++ app/src/main/java/com/aaa/permissionsample/MainActivity.kt
@@ -1,118 +1,26 @@
 package com.aaa.permissionsample
 
-import android.Manifest
-import android.app.Dialog
-import android.content.Context
-import android.content.SharedPreferences
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.annotation.StringRes
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.content.edit
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.DialogFragment
-
-const val PREFKEY_FIRST_LAUNCH = "PREFKEY_FIRST_LAUNCH"
+import com.aaa.permissionsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var sharedPref: SharedPreferences
+    private lateinit var _binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        sharedPref = getPreferences(Context.MODE_PRIVATE)
-
-        /* 権限(s)チェック */
-        val checkOK = checkPermissions()
-        if(checkOK) {
-            /* ★★★ここに権限チェックOK後の処理 */
-            Toast.makeText(this, "権限OK", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    /* 権限(s)チェック */
-    private fun checkPermissions(): Boolean {
-        /* 複数の権限チェック */
-        val permissions = arrayOf(Manifest.permission.POST_NOTIFICATIONS, /*複数時はここに並べる*/)
-        /* 未許可権限を取得 */
-        val deniedPermissions: MutableList<String> = ArrayList()
-        for (permission in permissions) {
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED)
-                deniedPermissions.add(permission)
-        }
-
-        /* 未許可権限チェック */
-        if(deniedPermissions.isEmpty())
-            return true  /* 権限付与済 何もする必要ない */
-
-        val fstLaunch = sharedPref.getBoolean(PREFKEY_FIRST_LAUNCH, true)
-        if(fstLaunch) {
-            /* 未許可権限があれば許可要求 */
-            permissionLauncher.launch(deniedPermissions.toTypedArray())
-            sharedPref.edit(commit=true) { putBoolean(PREFKEY_FIRST_LAUNCH, false)}
-        }
-        else {
-            val rationaleNeeded = deniedPermissions.any { permission ->
-                ActivityCompat.shouldShowRequestPermissionRationale(this, permission)
-            }
-
-            if(rationaleNeeded) {
-                /* 以前に拒否った */
-                permissionLauncher.launch(deniedPermissions.toTypedArray())
-            }
-            else {
-                /* 以前に"非表示にした"ならアラートダイアログ → Shutdown */
-                PermissionDialogFragment.show(this, R.string.wording_permission_2times)
-            }
-        }
-        return false
-    }
-
-    private val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
-        isGranted: Map<String, Boolean> ->
-            /* 権限チェック */
-            if (isGranted.all({it.value == true})) {
-                /* ★★★ここに権限チェックOK後の処理 */
-                Toast.makeText(this, "権限OK", Toast.LENGTH_SHORT).show()
-                return@registerForActivityResult    /* 全て許可済 問題なし */
-            }
-            else {
-                /* ひとつでも権限不足ありならアラートダイアログ→Shutdown */
-                PermissionDialogFragment.show(this, R.string.wording_permission)
-            }
-    }
-}
-
-class PermissionDialogFragment(@StringRes private val redid: Int) : DialogFragment() {
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val activity = requireActivity()
-        return AlertDialog.Builder(activity)
-            .setTitle(activity.getString(R.string.req_permission))
-            .setMessage(activity.getString(redid, activity.getString(R.string.app_name)))
-            .setPositiveButton("OK") { _, _ ->
-                activity.finish()
-            }
-            .create()
-    }
-
-    companion object {
-        fun show(activity: AppCompatActivity, @StringRes redid: Int) {
-            val fragment = PermissionDialogFragment(redid)
-            fragment.show(activity.supportFragmentManager, "PermissionDialog")
         }
     }
 }
---
+++ app/src/main/java/com/aaa/permissionsample/permissions/PermissionsFragment.kt
@@ -0,0 +1,104 @@
+package com.aaa.permissionsample.permissions
+
+import android.Manifest
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+import com.aaa.permissionsample.databinding.FragmentPermissionsBinding
+import com.aaa.permissionsample.R
+
+val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA, Manifest.permission.INTERNET)
+
+class PermissionsFragment : Fragment() {
+    private lateinit var _binding: FragmentPermissionsBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("aaaaa", "onCreate")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        Log.d("aaaaa", "onCreateView")
+        _binding = FragmentPermissionsBinding.inflate(inflater, container, false)
+        return _binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        Log.d("aaaaa", "onViewCreated 39")
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.d("aaaaa", "onViewCreated 42")
+        /* 権限承認済ならメイン処理のFragmentへ(移行このFragmentには戻らない) */
+        if (checkPermissionsGranted()) {
+            Log.d("aaaaa", "onViewCreated 45")
+            findNavController().navigate(R.id.action_permissionsFragment_to_mainFragment_fade1, null,
+                NavOptions.Builder().setPopUpTo(R.id.permissionsFragment, true).build()/* 戻る必要がない */)
+        }
+
+        Log.d("aaaaa", "onViewCreated 50")
+        permissionLauncher.launch(REQUIRED_PERMISSIONS)
+    }
+
+    private fun checkPermissionsGranted(): Boolean {
+        var result = true
+        for (permission in REQUIRED_PERMISSIONS) {
+            result = result && ContextCompat.checkSelfPermission(requireContext(), permission) == PackageManager.PERMISSION_GRANTED
+        }
+        return result
+    }
+
+    private val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            isGranted: Map<String, Boolean> ->
+        Log.d("aaaaa", "registerForActivityResult 64")
+        /* 権限チェック */
+        if (isGranted.isNotEmpty() && isGranted.all({it.value == true})) {
+            Log.d("aaaaa", "registerForActivityResult 67 count=${isGranted.count()} isGranted=${isGranted.entries.joinToString(separator = ","){ "${it.key}=${it.value}" }}")
+            /* 権限承認済ならメイン処理のFragmentへ(移行このFragmentには戻らない) */
+            findNavController().navigate(R.id.action_permissionsFragment_to_mainFragment_fade1, null,
+                NavOptions.Builder().setPopUpTo(R.id.permissionsFragment, true).build()/* 戻る必要がない */)
+        }
+        else {
+            Log.d("aaaaa", "registerForActivityResult 73")
+            /* ひとつでも権限不足ありならアラートダイアログ→Shutdown */
+            PermissionDialogFragment.show(requireActivity(), R.string.wording_permission)
+        }
+    }
+}
+
+class PermissionDialogFragment(@StringRes private val redid: Int) : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val activity = requireActivity()
+        return AlertDialog.Builder(activity)
+            .setTitle(activity.getString(R.string.req_permission))
+            .setMessage(activity.getString(redid, activity.getString(R.string.app_name)))
+            .setPositiveButton("OK") { _, _ ->
+                activity.finish()
+            }
+            .create()
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+        requireActivity().finish()
+    }
+
+    companion object {
+        fun show(activity: FragmentActivity, @StringRes redid: Int) {
+            val fragment = PermissionDialogFragment(redid)
+            fragment.show(activity.supportFragmentManager, "PermissionDialog")
+        }
+    }
+}
